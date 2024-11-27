3b7afd009f421440989e9d144b073ed182035530
rin! <phurinmeng@gmail.com>
2024-11-26 19:08:21 -0800

    login page done, now moving on to back end for login (and registering)

--- app/src/main/java/com/example/hikelowna/MainActivity.java
+++ app/src/main/java/com/example/hikelowna/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.hikelowna;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginButton,registerButton;
+    Intent it;
+    CheckBox rememberMe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
+        rememberMe = findViewById(R.id.rememberMe);
+
+        loginButton.setOnClickListener(view -> {
+            it = new Intent(MainActivity.this, LandingPage.class);
+            startActivity(it);
+        });
+
+        registerButton.setOnClickListener(view -> {
+            it = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(it);
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
